6c566046e07c5959f0255d358da2999786a7edf0
Salinas <Horacio.Salinas@nidec-motor.com>
2023-01-24 08:39:22 -0600

    repo

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('api.json')
+        const data = await res.json
+    } catch (error) {
+        console.log(error)
+    }
+}
